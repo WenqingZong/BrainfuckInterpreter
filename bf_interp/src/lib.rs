926300c728ded820c08f209d83730f85313a29f6
Wenqing Zong <wenqing.zong@codethink.co.uk>
2023-04-23 19:30:41 +0100

    sesson6, can finally interprete hello world

--- bf_interp/src/lib.rs
+++ bf_interp/src/lib.rs
@@ -1,11 +1,14 @@
 //! A representation of Brainfuck virtual machine and the actual interpret functions.
 
+pub mod auto_newline_writer;
 pub mod brainfuck_runtime_error;
 pub mod cell_kind;
 
+use auto_newline_writer::AutoNewlineWriter;
 use bf_types::{Program, RawInstruction};
 use brainfuck_runtime_error::BrainfuckRuntimeError;
 use cell_kind::CellKind;
+use std::collections::HashMap;
 use std::io::{Read, Write};
 use std::num::NonZeroUsize;
 
@@ -20,6 +23,8 @@ where
     can_extend: bool,
     program_counter: usize,
     program: &'a Program,
+    open_to_close: HashMap<usize, usize>,
+    close_to_open: HashMap<usize, usize>,
 }
 
 impl<'a, T> VM<'a, T>
@@ -33,6 +38,7 @@ where
     /// `can_extend` specifies if the memory can extend when it's full.
     ///
     /// `program` is a borrow to a [Program] struct which this [VM] will later interpret.
+    /// It is assumed that `program` is a valid one, i.e., it can pass `program.validate();`
     /// # Example
     /// ```no_run
     /// # use bf_interp::*;
@@ -48,12 +54,34 @@ where
     pub fn new(memory_size: NonZeroUsize, can_extend: bool, program: &'a Program) -> VM<'a, T> {
         let mut memory: Vec<T> = vec![];
         memory.resize(memory_size.get(), T::zero());
+
+        // Construct matching brackets.
+        let mut open_to_close: HashMap<usize, usize> = HashMap::new();
+        let mut stack: Vec<usize> = Vec::with_capacity(program.instructions().len());
+        for (idx, ins) in program.instructions().iter().enumerate() {
+            if ins.raw_instruction() == RawInstruction::BeginLoop {
+                stack.push(idx);
+            } else if ins.raw_instruction() == RawInstruction::EndLoop {
+                let open_idx = stack.pop().unwrap();
+                open_to_close.insert(open_idx, idx);
+            }
+        }
+
+        // Construct the reverse HashMap.
+        let mut close_to_open: HashMap<usize, usize> = HashMap::new();
+        for (open_idx, close_idx) in open_to_close.iter() {
+            close_to_open.insert(*close_idx, *open_idx);
+        }
+
+        // Construct the VM.
         Self {
             memory,
             pointer: 0,
             can_extend,
             program_counter: 0,
             program,
+            open_to_close,
+            close_to_open,
         }
     }
 
@@ -79,24 +107,26 @@ where
         read_source: &mut R,
         write_destination: &mut W,
     ) -> Result<(), BrainfuckRuntimeError> {
-        for (i, instruction) in self.program.instructions().iter().enumerate() {
-            self.program_counter = i;
-            match instruction.raw_instruction() {
-                RawInstruction::MoveLeft => self.move_left()?,
-                RawInstruction::MoveRight => self.move_right()?,
+        let mut auto_newline_writer = AutoNewlineWriter::new(write_destination);
+        while self.program_counter < self.program.instructions().len() {
+            let instruction = self.program.instructions()[self.program_counter];
+            self.program_counter = match instruction.raw_instruction() {
+                RawInstruction::MoveLeft => self.move_left(),
+                RawInstruction::MoveRight => self.move_right(),
                 RawInstruction::Increment => self.increment(),
                 RawInstruction::Decrement => self.decrement(),
-                RawInstruction::Input => self.read_value(read_source)?,
-                RawInstruction::Output => self.write_value(write_destination)?,
-                _ => println!("{instruction:?}"),
-            }
+                RawInstruction::Input => self.read_value(read_source),
+                RawInstruction::Output => self.write_value(&mut auto_newline_writer),
+                RawInstruction::BeginLoop => self.begin_loop(),
+                RawInstruction::EndLoop => self.end_loop(),
+            }?
         }
         Ok(())
     }
 
     /// Move [VM] pointer one place to the left. Will cause a [BrainfuckRuntimeError] if the pointer is already at
     /// position 0.
-    fn move_left(&mut self) -> Result<(), BrainfuckRuntimeError> {
+    fn move_left(&mut self) -> Result<usize, BrainfuckRuntimeError> {
         if self.pointer == 0 {
             return Err(BrainfuckRuntimeError::CannotMoveLeftError(
                 self.program.file_path().to_owned(),
@@ -104,13 +134,13 @@ where
             ));
         }
         self.pointer -= 1;
-        Ok(())
+        Ok(self.program_counter + 1)
     }
 
     /// Move [VM] pointer one place to the right. If the pointer is already at the right most position, then this method
     /// will either double [VM]'s memory size if it's allowed, or return a [BrainfuckRuntimeError] saying invalid
     /// operation.
-    fn move_right(&mut self) -> Result<(), BrainfuckRuntimeError> {
+    fn move_right(&mut self) -> Result<usize, BrainfuckRuntimeError> {
         let memory_size = self.memory.len();
 
         if self.pointer == memory_size - 1 && !self.can_extend {
@@ -123,21 +153,26 @@ where
         }
 
         self.pointer += 1;
-        Ok(())
+        Ok(self.program_counter + 1)
     }
 
     /// Increment the value pointed by [VM] pointer.
-    fn increment(&mut self) {
+    fn increment(&mut self) -> Result<usize, BrainfuckRuntimeError> {
         self.memory[self.pointer].increment();
+        Ok(self.program_counter + 1)
     }
 
     /// Decrement the value pointed by [VM] pointer.
-    fn decrement(&mut self) {
+    fn decrement(&mut self) -> Result<usize, BrainfuckRuntimeError> {
         self.memory[self.pointer].decrement();
+        Ok(self.program_counter + 1)
     }
 
     /// Read a u8 value from user specified reading source. Anything beyond a byte-long would be ignored.
-    fn read_value<R: Read>(&mut self, input_source: &mut R) -> Result<(), BrainfuckRuntimeError> {
+    fn read_value<R: Read>(
+        &mut self,
+        input_source: &mut R,
+    ) -> Result<usize, BrainfuckRuntimeError> {
         let mut buf = [0; 1];
         input_source.read_exact(&mut buf).map_err(|e| {
             BrainfuckRuntimeError::CannotReadInputError(
@@ -149,14 +184,14 @@ where
 
         self.memory[self.pointer].set_value(buf[0]);
 
-        Ok(())
+        Ok(self.program_counter + 1)
     }
 
     /// Write a cell value as ASCII to user specified write destination.
     fn write_value<W: Write>(
         &self,
         write_destination: &mut W,
-    ) -> Result<(), BrainfuckRuntimeError> {
+    ) -> Result<usize, BrainfuckRuntimeError> {
         let value = self.memory()[self.pointer].get_value();
         write_destination.write_all(&[value]).map_err(|e| {
             BrainfuckRuntimeError::CannotWriteOutputError(
@@ -166,7 +201,26 @@ where
             )
         })?;
 
-        Ok(())
+        Ok(self.program_counter + 1)
+    }
+
+    /// Start a loop for Brainfuck code.
+    fn begin_loop(&mut self) -> Result<usize, BrainfuckRuntimeError> {
+        if self.memory[self.pointer] == T::zero() {
+            Ok(self.open_to_close.get(&self.program_counter).unwrap() + 1)
+        } else {
+            Ok(self.program_counter + 1)
+        }
+    }
+
+    /// End the current Brainfuck code loop.
+    fn end_loop(&mut self) -> Result<usize, BrainfuckRuntimeError> {
+        // Ok(*self.close_to_open.get(&self.program_counter).unwrap())
+        if self.memory[self.pointer] != T::zero() {
+            Ok(self.close_to_open.get(&self.program_counter).unwrap() + 1)
+        } else {
+            Ok(self.program_counter + 1)
+        }
     }
 
     /// Getter.
@@ -361,6 +415,55 @@ mod tests {
         let result = virtual_machine.interpret(&mut stdin(), &mut write_destination);
 
         assert!(result.is_ok());
-        assert_eq!(write_destination.into_inner(), vec![65]);
+        assert_eq!(write_destination.into_inner(), vec![65, b'\n']);
+    }
+
+    /// Should construct matching brackets.
+    #[test]
+    fn should_construct_matching_brackets() {
+        let program = Program::new("", "[]");
+        let virtual_machine: VM<u8> = VM::new(NonZeroUsize::new(2).unwrap(), false, &program);
+
+        let mut expected_open_to_close: HashMap<usize, usize> = HashMap::new();
+        expected_open_to_close.insert(0, 1);
+        let mut expected_close_to_open: HashMap<usize, usize> = HashMap::new();
+        expected_close_to_open.insert(1, 0);
+
+        assert_eq!(virtual_machine.open_to_close, expected_open_to_close);
+        assert_eq!(virtual_machine.close_to_open, expected_close_to_open);
+    }
+
+    /// Should move program counter to the next instruction after end loop.
+    #[test]
+    fn should_move_program_counter_to_next_ins_after_end_loop() {
+        let program = Program::new("", "[]");
+        let mut virtual_machine: VM<u8> = VM::new(NonZeroUsize::new(2).unwrap(), false, &program);
+        let result = virtual_machine.interpret(&mut stdin(), &mut stdout());
+
+        assert!(result.is_ok());
+        assert_eq!(virtual_machine.program_counter, 2);
+    }
+
+    /// Should move program counter to left.
+    #[test]
+    fn should_move_program_counter_to_left() {
+        let program = Program::new("", "+[]");
+        let mut virtual_machine: VM<u8> = VM::new(NonZeroUsize::new(2).unwrap(), false, &program);
+        virtual_machine.program_counter = virtual_machine.increment().unwrap();
+        virtual_machine.program_counter = virtual_machine.begin_loop().unwrap();
+
+        assert_eq!(virtual_machine.program_counter, 2);
+    }
+
+    /// Should move program counter back to loop start plus 1.
+    #[test]
+    fn should_move_program_counter_back_to_loop_start_plus_1() {
+        let program = Program::new("", "+[]");
+        let mut virtual_machine: VM<u8> = VM::new(NonZeroUsize::new(2).unwrap(), false, &program);
+        virtual_machine.program_counter = virtual_machine.increment().unwrap();
+        virtual_machine.program_counter = virtual_machine.begin_loop().unwrap();
+        virtual_machine.program_counter = virtual_machine.end_loop().unwrap();
+
+        assert_eq!(virtual_machine.program_counter, 2);
     }
 }
